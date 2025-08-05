6a4052dfe2b9cec49d50001188b7863ea38e9d09
AJaswani39 <jaswanianagha8@gmail.com>
2025-08-05 14:53:57 -0400

    Added Restaurant.java class
    Added class for Restaurant (that I plan to later extract utilizing Google Maps API)

--- app/src/main/java/com/example/FGluten/data/Restaurant.java
+++ app/src/main/java/com/example/FGluten/data/Restaurant.java
@@ -1,5 +1,64 @@
 package com.example.FGluten.data;
 
+import java.util.List;
+import java.util.Objects;
+
+//GF = Gluten free
 public class Restaurant {
+    private final String name;
+    private final String address;
+    private final boolean hasGFMenu;
+    private final List<String> gfMenu;
+
+    private final double latitude;
+    private final double longitude;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public List<String> getGlutenFreeMenu() {
+        return gfMenu;
+    }
+
+    public boolean gethasGFMenu() {
+        return hasGFMenu;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public String determineIfGlutenFree(Object gf) {
+        if (!this.hasGFMenu) {
+            return "This restaurant has no gluten free options";
+        } else {
+            return "This restaurant has gluten free options";
+        }
+    }
+
+
+    // toString method
+    @Override
+    public String toString(){
+        return "Restaurant{" +
+                "name=" + name
+                + " address=" + address
+                + " hasGlutenFreeOption=" + hasGFMenu
+                + " Gluten Free Menu=" + gfMenu
+                + " latitude=" + latitude
+                + " longitude=" + longitude
+                + "}";
+    }
 
 }
