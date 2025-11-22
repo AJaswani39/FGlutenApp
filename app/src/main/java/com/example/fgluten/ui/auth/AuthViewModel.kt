bc06b9c00e9dab45f5b713535b252b39ba86c85f
AJaswani39 <jaswanianagha8@gmail.com>
2025-11-22 12:57:43 -0500

    Troubleshooting crash on clicking sign in button

--- app/src/main/java/com/example/fgluten/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/fgluten/ui/auth/AuthViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.fgluten.ui.auth
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
